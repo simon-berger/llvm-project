1cad42d9b8c5c64d3eb2ddf07c190f9792185524
Simon Berger <simon.c.berger@pm.me>
2023-01-18 18:17:30 +0100

    Add own hello world test pass

--- llvm/lib/Transforms/Utils/HelloWorld.cpp
+++ llvm/lib/Transforms/Utils/HelloWorld.cpp
@@ -1,11 +1,3 @@
-//===-- HelloWorld.cpp - Example Transformations --------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "llvm/Transforms/Utils/HelloWorld.h"
 
 using namespace llvm;
